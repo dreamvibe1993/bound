fdd1b1a7a9afc963ee342cfd28d43942a25639d9
George Apraksin <dreamvibe@mail.ru>
2023-07-01 17:03:00 +0500

    1.0.0

--- bound.py
+++ bound.py
@@ -5,16 +5,39 @@ from re import Match
 from typing import Union
 
 mobx_reserved_words: "list[str]" = ['observable', 'action.bound', 'computed']
+say = print
+PLACEHOLDER = 'PLACEHOLDER'
+
+
+def is_file_already_has_bound_mobx_vars(ts_file: str) -> bool:
+	return len(get_existing_mobx_observables_obj(ts_file)) > 0
+
+
+def update_mobx_bounds_object(ts_file: str) -> str:
+	if not ts_file: return ""
+	ts_file_copy = ts_file
+	ts_file_bounds_object_cut: str = re.sub(
+		r"(export\s)?const\s\w+(Observables)\s=\s{[^/]*?};", PLACEHOLDER, ts_file_copy)
+	ts_class: str = get_ts_class(ts_file_copy)
+	updated_ts_file = ts_file_bounds_object_cut.replace(PLACEHOLDER, create_mobx_observables_js_object(ts_class))
+	return updated_ts_file
+
+
+def paste_mobx_bounds_object(ts_file: str) -> str:
+	if not ts_file: return ""
+	ts_file_copy = ts_file
+	ts_class: str = get_ts_class(ts_file_copy)
+	updated_ts_file = ts_file_copy.replace(ts_class, create_mobx_observables_js_object(ts_class) + "\n" + ts_class)
+	return updated_ts_file
 
 
 # Process strings
-
 def get_class_name(ts_class: str) -> str:
 	class_name = re.search(r"(?<=class\s)\w+", ts_class)
 	return class_name.group(0) if class_name else ts_class
 
 
-def create_bounds(actions: "list[str]") -> "list[str]":
+def create_action_bounds(actions: "list[str]") -> "list[str]":
 	list_of_actions_bound: list[str] = []
 	for action in actions:
 		list_of_actions_bound.append(f"{action}: action.bound")
@@ -50,9 +73,10 @@ def create_mobx_observables_js_object(ts_class) -> str:
 		computeds = ',\n'.join(f'    {c}' for c in create_computeds(getters_setters))
 		computeds_str = f"\n{computeds},\n"
 	if len(actions) > 0:
-		actions_bound = ',\n'.join(f'    {a}' for a in create_bounds(actions))
+		actions_bound = ',\n'.join(f'    {a}' for a in create_action_bounds(actions))
 		actions_bound_str = f'\n{actions_bound},\n'
 	obj = f'export const {class_name[0].lower() + class_name[1:]}Observables = {{{observables_str}{computeds_str}{actions_bound_str}}};'
+	print(obj)
 	return obj
 
 
@@ -60,15 +84,6 @@ def get_all_observables_in_class(ts_class: str) -> "list[str]":
 	def cut_all_before_constructor(ts_class_string: str) -> str:
 		cut = re.search(r"(?<={)[^%]*(?=constructor)", ts_class_string)
 		return cut.group(0) if cut else ts_class_string
-
-	def get_rid_of_comments(piece: str) -> str:
-		return re.sub(r"/.*?\*/", "", piece, flags=re.DOTALL)
-
-	def cut_everything_after_eq_sign(piece: str) -> str:
-		return re.sub(r"\s=.+?;", ";", piece, flags=re.DOTALL)
-
-	def cut_everything_after_ddot(piece: str) -> str:
-		return re.sub(r":.+?(?=;)", "", piece, flags=re.DOTALL)
 
 	def cut_reserved_words(piece: str) -> str:
 		observables: list[str] = piece.split(";")
@@ -82,9 +97,9 @@ def get_all_observables_in_class(ts_class: str) -> "list[str]":
 
 	ts_class_copy = ts_class
 	ts_class_copy: str = cut_all_before_constructor(ts_class_copy)
-	ts_class_copy: str = get_rid_of_comments(ts_class_copy)
-	ts_class_copy: str = cut_everything_after_eq_sign(ts_class_copy)
-	ts_class_copy: str = cut_everything_after_ddot(ts_class_copy)
+	ts_class_copy: str = re.sub(r"/.*?\*/", "", ts_class_copy, flags=re.DOTALL)
+	ts_class_copy: str = re.sub(r"\s=.+?;", ";", ts_class_copy, flags=re.DOTALL)
+	ts_class_copy: str = re.sub(r":.+?(?=;)", "", ts_class_copy, flags=re.DOTALL)
 	ts_class_copy: str = cut_reserved_words(ts_class_copy)
 	observables: list[str] = []
 	for observable in ts_class_copy.split(";"):
@@ -110,7 +125,6 @@ def get_all_actions_in_class(ts_class: str) -> "list[str]":
 	methods: list[str] = []
 	for d_method in dirty_methods:
 		methods.append(re.sub(r"[(){\n\r\s]", "", d_method))
-	print(methods)
 	return sorted(methods)
 
 
@@ -119,23 +133,25 @@ def get_all_computeds_in_class(ts_class: str) -> "list[str]":
 	return sorted(computeds)
 
 
-def get_class_model(ts_file: str) -> str:
+def get_ts_class(ts_file: str) -> str:
 	class_model = re.search(r"(export\s)?class\s[A-Z]\w+[^%]*}", ts_file)
 	return class_model.group(0) if class_model else ts_file
 
 
 def get_existing_mobx_observables_obj(model_str: str) -> str:
-	if 'Observables' not in model_str:
-		print('Кажется, в этом файле нет объекта, оканчивающегося на Observables. Вы правильно назвали объект?')
 	observables_js_obj: Union[Match[bytes], None, Match[str]] = re.search(
 		r"(export\s)?const\s\w+(Observables)\s=\s{[^/]*?};",
 		model_str)
-	if not observables_js_obj: return ""
-	string_found = observables_js_obj.group(0)
+	if not observables_js_obj:
+		say('Не удалось найти объект с переменными. Причина: ')
+		say('Кажется, в этом файле нет объекта, оканчивающегося на Observables. Вы правильно назвали объект?')
+		return ""
+
+	string_found: str = observables_js_obj.group(0)
 	is_real_mobx_obj: bool = False
-	for r_word in mobx_reserved_words:
+	for reserved_word in mobx_reserved_words:
 		if is_real_mobx_obj: break
-		is_real_mobx_obj = r_word in string_found
+		is_real_mobx_obj = reserved_word in string_found
 	if not is_real_mobx_obj: return ""
 	return string_found
 
@@ -146,29 +162,39 @@ def parse_file(file_path: str) -> str:
 		with open(file_path, 'r') as file:
 			return file.read()
 	except FileNotFoundError:
-		print(f"Ошибка: Файл '{file_path}' не найден.")
+		say(f"Ошибка: Файл '{file_path}' не найден.")
 	except Exception as e:
-		print(f"Произошла ошибка при обработке файла '{file_path}': {str(e)}")
+		say(f"Произошла ошибка при обработке файла '{file_path}': {str(e)}")
 
 
-def process_file(file_path):
+def process_file(file_path) -> str:
 	try:
 		ts_file = parse_file(file_path)
+		result: str = ""
 		if ts_file is not None:
-			print("Working...", ts_file)
+			if is_file_already_has_bound_mobx_vars(ts_file):
+				result = update_mobx_bounds_object(ts_file)
+			else:
+				result = paste_mobx_bounds_object(ts_file)
+		return result
 	except Exception as e:
-		print(f"Произошла ошибка при обработке файла '{file_path}': {str(e)}")
+		say(f"Произошла ошибка при обработке файла '{file_path}': {str(e)}")
 
 
-parser = argparse.ArgumentParser(
-	description='Синхронизация mobx observables моделей')
-parser.add_argument('path', help='Путь к директории или файлу TS.')
+def handle_finish(content, file_path) -> None:
+	with open(file_path, 'w') as annotated_file:
+		annotated_file.write(content)
+	print(f"Файл сохранен как: {file_path}")
 
-args = parser.parse_args()
-path: str = args.path
 
-if os.path.isfile(path) and path.endswith('.ts'):
-	process_file(path)
-else:
-	print(
-		f"Указанный путь '{path}' не является файлом TS.")
+def main():
+	parser = argparse.ArgumentParser(
+		description='Синхронизация mobx observables моделей')
+	parser.add_argument('path', help='Путь к директории или файлу TS.')
+	args = parser.parse_args()
+	path: str = args.path
+	if os.path.isfile(path) and path.endswith('.ts'):
+		handle_finish(process_file(path), path)
+	else:
+		say(
+			f"Указанный путь '{path}' не является файлом TS.")
