50a7eb3145611f75feb944a80aba5223a4f6477d
George Apraksin <dreamvibe@mail.ru>
2023-07-03 00:39:19 +0500

    1.1.0 Предупреждение пользователя, если существует возможность того, что скрипт "забыл" добавить какую-нибудь переменную

--- bound.py
+++ bound.py
@@ -5,13 +5,33 @@ from re import Match
 from typing import Union
 
 mobx_reserved_words: "list[str]" = ['observable', 'action.bound', 'computed']
-say = print
+class_entites_declarations_regex: str = r"(private\s|abstract\s|protected\s)?"
+computeds_words_regex: str = r"(get\s|set\s)"
 PLACEHOLDER = 'PLACEHOLDER'
 
+say = print
+
+
+# Asserters
 
 def is_file_already_has_bound_mobx_vars(ts_file: str) -> bool:
 	return len(get_existing_mobx_observables_obj(ts_file)) > 0
 
+
+def is_operation_done_correctly(methods: "list[str]", variables: "list[str]", getters_setters: "list[str]",
+                                ts_file: str) -> bool:
+	ts_class = cut_constructor(delete_js_docs(get_ts_class(ts_file)))
+	for g in getters_setters:
+		ts_class = re.sub(fr"(get|set)\s{g}\([^%]+?}}\n", "", ts_class)
+	for v in variables:
+		ts_class = re.sub(fr"{class_entites_declarations_regex}{v}.*;\n", "", ts_class, flags=re.DOTALL | re.MULTILINE)
+	for m in methods:
+		ts_class = re.sub(fr"{m}.*?}}\n", "", ts_class, flags=re.DOTALL | re.MULTILINE)
+	ts_class = re.sub(r"[\s\n\t\r]*", "", ts_class)
+	return bool(re.search(r"(?<={)\w+(?=})", ts_class)) is False
+
+
+# Processors
 
 def update_mobx_bounds_object(ts_file: str) -> str:
 	if not ts_file: return ""
@@ -31,11 +51,7 @@ def paste_mobx_bounds_object(ts_file: str) -> str:
 	return updated_ts_file
 
 
-# Process strings
-def get_class_name(ts_class: str) -> str:
-	class_name = re.search(r"(?<=class\s)\w+", ts_class)
-	return class_name.group(0) if class_name else ts_class
-
+# Creators
 
 def create_action_bounds(actions: "list[str]") -> "list[str]":
 	list_of_actions_bound: list[str] = []
@@ -60,9 +76,11 @@ def create_computeds(computeds: "list[str]") -> "list[str]":
 
 def create_mobx_observables_js_object(ts_class) -> str:
 	class_name = get_class_name(ts_class)
-	variables: list[str] = get_all_observables_in_class(ts_class)
+	variables: list[str] = get_all_variables_in_class(ts_class)
 	actions: list[str] = get_all_actions_in_class(ts_class)
 	getters_setters: list[str] = get_all_computeds_in_class(ts_class)
+	if is_operation_done_correctly(actions, variables, getters_setters, ts_class) is False:
+		say(f'Возможно, скрипт что-то упустил. Проверь переменные у {class_name}!')
 	observables_str: str = ""
 	actions_bound_str: str = ""
 	computeds_str = ""
@@ -79,53 +97,77 @@ def create_mobx_observables_js_object(ts_class) -> str:
 	return mobx_object
 
 
+# Cutters
+def cut_all_before_constructor(ts_class_string: str) -> str:
+	cut = re.search(r"(?<={)[^%]*(?=constructor)", ts_class_string)
+	return cut.group(0) if cut else ts_class_string
+
+
+def cut_reserved_words(ts_class_cut: str) -> str:
+	variables: list[str] = ts_class_cut.split(";")
+	variables_processed: list[str] = []
+	for variable in variables:
+		if "abstract" in variable: continue
+		variable_processed = re.sub(r"\w+(?=\s)", "", variable)
+		variables_processed.append(variable_processed.strip())
+	all_variables_string = re.sub(r"(\n*|\s*)", "", ";".join(variables_processed).strip())
+	return all_variables_string
+
+
+def cut_constructor(ts_class) -> str:
+	return re.sub(r"constructor.+?}", "", ts_class, flags=re.DOTALL)
+
+
+def cut_variable(ts_class: str, variable: str) -> str:
+	return re.sub(fr"(?<=\s\s){class_entites_declarations_regex}{variable}.*", "", ts_class)
+
+
+# Deleters
+
 def delete_all_what_is_commented(ts_file: str) -> str:
 	no_comments = re.sub(r"//.*", "", ts_file, re.DOTALL)
 	return no_comments if len(no_comments) > 0 else ts_file
 
 
-def get_all_observables_in_class(ts_class: str) -> "list[str]":
-	def cut_all_before_constructor(ts_class_string: str) -> str:
-		cut = re.search(r"(?<={)[^%]*(?=constructor)", ts_class_string)
-		return cut.group(0) if cut else ts_class_string
+def delete_js_docs(ts_file: str) -> str:
+	return re.sub(r"/.*?\*/", "", ts_file, flags=re.DOTALL)
 
-	def cut_reserved_words(piece: str) -> str:
-		observables: list[str] = piece.split(";")
-		observables_processed: list[str] = []
-		for observable in observables:
-			if "abstract" in observable: continue
-			observable_processed = re.sub(r"\w+(?=\s)", "", observable)
-			observables_processed.append(observable_processed.strip())
-		all_observables_to_string = re.sub(r"(\n*|\s*)", "", ";".join(observables_processed).strip())
-		return all_observables_to_string
 
+# Getters
+
+def get_class_name(ts_class: str) -> str:
+	class_name = re.search(r"(?<=class\s)\w+", ts_class)
+	return class_name.group(0) if class_name else ts_class
+
+
+def get_all_variables_in_class(ts_class: str) -> "list[str]":
 	ts_class_copy = delete_all_what_is_commented(ts_class)
 	ts_class_copy: str = cut_all_before_constructor(ts_class_copy)
-	ts_class_copy: str = re.sub(r"/.*?\*/", "", ts_class_copy, flags=re.DOTALL)
+	ts_class_copy: str = delete_js_docs(ts_class_copy)
 	ts_class_copy: str = re.sub(r"\s=.+?;", ";", ts_class_copy, flags=re.DOTALL)
 	ts_class_copy: str = re.sub(r":.+?(?=;)", "", ts_class_copy, flags=re.DOTALL)
 	ts_class_copy: str = cut_reserved_words(ts_class_copy)
-	observables: list[str] = []
-	for observable in ts_class_copy.split(";"):
-		if observable != "" and observable not in observables:
-			observables.append(re.sub(r"\W*", "", observable))
-	return sorted(observables)
+	variables: list[str] = []
+	for variable in ts_class_copy.split(";"):
+		if variable != "" and variable not in variables:
+			variables.append(re.sub(r"\W*", "", variable))
+	return sorted(variables)
 
 
 def get_all_actions_in_class(ts_class: str) -> "list[str]":
-	observables = get_all_observables_in_class(ts_class)
+	variables = get_all_variables_in_class(ts_class)
 	ts_class_copy = delete_all_what_is_commented(ts_class)
-	for observable in observables:
-		ts_class_copy = re.sub(fr"(?<=\s\s)(private|abstract|protected)?\s{observable}.*", "", ts_class_copy)
+	for variable in variables:
+		ts_class_copy = cut_variable(ts_class_copy, variable)
 	ts_class_copy = re.sub(r"^.*class\s.+{", "", ts_class_copy, re.DOTALL).strip()
-	ts_class_copy = re.sub(r"constructor[^}]*}", "", ts_class_copy, re.DOTALL)
+	ts_class_copy = cut_constructor(ts_class_copy)
 	ts_class_copy = re.sub(r"async\s", "", ts_class_copy, re.DOTALL)
-	ts_class_copy = re.sub(r"(get|set)\s[^}]*}", "", ts_class_copy, re.DOTALL)
+	ts_class_copy = re.sub(fr"{computeds_words_regex}[^}}]*}}", "", ts_class_copy, re.DOTALL)
 	ts_class_copy = re.sub(r"(?<=\)):[^}]*?(?={)", "", ts_class_copy, re.DOTALL)
 	ts_class_copy = re.sub(r"\(.*?\)", "()", ts_class_copy, flags=re.DOTALL)
 	ts_class_copy = re.sub(r":[^%\n]+?{", "()", ts_class_copy, re.DOTALL)
 	unprocessed_methods: list[str] = re.findall(r"(?<!\w\s)(?<=\s\s)\w+\(\)[^;),]{?\n?", ts_class_copy,
-	                                      flags=re.DOTALL | re.MULTILINE)
+	                                            flags=re.DOTALL | re.MULTILINE)
 	methods: list[str] = []
 	for unprocessed_method in unprocessed_methods:
 		methods.append(re.sub(r"[(){\n\r\s]", "", unprocessed_method))
--- utils/utils.py
+++ utils/utils.py
@@ -2,4 +2,4 @@ import re
 
 
 def fmt(s: str) -> str:
-	return re.sub(r"[\s\n\t\r]*", "", s)
+	return re.sub(r"[\s\n\t\r]*", "", s) if type(s) is str else ""
